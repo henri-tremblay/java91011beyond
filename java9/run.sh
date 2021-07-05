a31592f84decb9eae0e90fcad765f343b3451349
Henri Tremblay <Henri.Tremblay@tradingscreen.com>
2021-07-04 23:12:01 -0400

    Other options for illegal-access

--- java9/run.sh
+++ java9/run.sh
@@ -12,7 +12,7 @@ java -cp app/target/classes:lib/target/classes app.App
 
 #java --module-path app/target/classes:lib/target/classes -m pro.tremblay.java9.app/app.App
 
-# --illegal-access=permit
+# --illegal-access=permit deny warn
 # --add-opens pro.tremblay.java9.lib/lib=pro.tremblay.java9.app
 # --add-opens java.base/java.lang=ALL-UNNAMED
 # --add-opens java.base/java.lang=pro.tremblay.java9.app
