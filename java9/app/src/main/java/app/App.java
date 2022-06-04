32c97c810e575d587e81ccd734029844b1cc89ab
Henri Tremblay <henri.tremblay@gmail.com>
2022-06-03 22:41:12 -0400

    Add missing import

--- java9/app/src/main/java/app/App.java
+++ java9/app/src/main/java/app/App.java
@@ -9,6 +9,7 @@ import lib.HelloStdout;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Field;
+import java.util.ServiceLoader;
 
 public class App {
   public static void main(String... args) throws Throwable {
