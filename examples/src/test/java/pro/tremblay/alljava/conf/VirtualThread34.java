3eeef27d6b0bf208ea118602f9278d7560cfbf9d
Henri Tremblay <henri.tremblay@gmail.com>
2023-08-20 00:54:15 -0400

    Virtual threads

---
+++ examples/src/test/java/pro/tremblay/alljava/conf/VirtualThread34.java
@@ -0,0 +1,26 @@
+package pro.tremblay.alljava.conf;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class VirtualThread34 {
+
+  @Test
+  public void test() {
+    for (int i = 0; i < 100_000; i++) {
+      System.out.println("Launch thread " + i);
+      new Thread(() -> {
+        try {
+          TimeUnit.MINUTES.sleep(1);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }).start();
+    }
+  }
+
+  // run with threads
+  // startVirtualThread
+  // pool, executor
+}
---
+++ examples/src/test/java/pro/tremblay/alljava/solutions/VirtualThread34.java
@@ -0,0 +1,27 @@
+package pro.tremblay.alljava.solutions;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+
+public class VirtualThread34 {
+
+  volatile boolean first = true;
+
+  @Test
+  public void test() throws Exception {
+    for (int i = 0; i < 100_000; i++) {
+      System.out.println("Launch thread " + i);
+      Thread.startVirtualThread(() -> {
+        try {
+          TimeUnit.MINUTES.sleep(1);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      });
+    }
+  }
+}
