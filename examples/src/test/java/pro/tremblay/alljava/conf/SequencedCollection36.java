88f420d3fa10278c72e6a99b06fe3782e8c56f84
Henri Tremblay <henri.tremblay@gmail.com>
2023-09-11 15:08:49 -0400

    Add sequence collections

--- examples/src/test/java/pro/tremblay/alljava/conf/SequencedCollection36.java
+++ examples/src/test/java/pro/tremblay/alljava/conf/SequencedCollection36.java
@@ -1,2 +1,23 @@
-package pro.tremblay.alljava.conf;public class SequencedCollection36 {
+package pro.tremblay.alljava.conf;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.SequencedCollection;
+import java.util.stream.Collectors;
+
+public class SequencedCollection36 {
+
+  @Test
+  public void test() {
+    SequencedCollection<String> list = List.of("I", "love", "Java");
+    list.addFirst("Sure,");
+    printList(list);
+    list.reversed();
+    printList(list);
+  }
+
+  private void printList(SequencedCollection<String> list) {
+    System.out.println(list.stream().collect(Collectors.joining(" ")));
+  }
 }
