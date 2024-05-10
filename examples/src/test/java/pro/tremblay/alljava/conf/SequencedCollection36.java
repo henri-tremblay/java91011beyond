835ee0ad6a1cfb1c592f825d1b662ad1971fffd1
Henri Tremblay <henri.tremblay@gmail.com>
2024-05-09 22:01:29 -0400

    Make addFirst work

--- examples/src/test/java/pro/tremblay/alljava/conf/SequencedCollection36.java
+++ examples/src/test/java/pro/tremblay/alljava/conf/SequencedCollection36.java
@@ -5,15 +5,15 @@ package pro.tremblay.alljava.conf;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.SequencedCollection;
-import java.util.stream.Collectors;
 
 public class SequencedCollection36 {
 
   @Test
   public void test() {
-    SequencedCollection<String> list = List.of("I", "love", "Java");
+    SequencedCollection<String> list = new ArrayList<>(List.of("I", "love", "Java"));
     list.addFirst("Sure,");
     printList(list);
     list.reversed();
@@ -21,6 +21,6 @@ public class SequencedCollection36 {
   }
 
   private void printList(SequencedCollection<String> list) {
-    System.out.println(list.stream().collect(Collectors.joining(" ")));
+    System.out.println(String.join(" ", list));
   }
 }
