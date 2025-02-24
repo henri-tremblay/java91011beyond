65ff297a77f3ebac6783d393ffa7a08f6315797f
Henri Tremblay <henri.tremblay@gmail.com>
2025-02-24 14:02:17 -0500

    Unused import

--- examples/src/test/java/pro/tremblay/alljava/conf/SimpleWebServer31.java
+++ examples/src/test/java/pro/tremblay/alljava/conf/SimpleWebServer31.java
@@ -14,7 +14,6 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
-import java.util.concurrent.CompletableFuture;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
