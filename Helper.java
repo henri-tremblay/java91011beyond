0ec776182883590ca43ad177a72ac1059c27e989
Henri Tremblay <henri.tremblay@gmail.com>
2025-04-29 23:55:53 -0400

    Add licenses

--- Helper.java
+++ Helper.java
@@ -1,3 +1,6 @@
+/*
+ * Copyright 2018-2025 Henri Tremblay.
+ */
 public class Helper {
   public static void hello() {
     System.out.println("Hello all");
--- LaunchMultiFile41.java
+++ LaunchMultiFile41.java
@@ -1,3 +1,6 @@
+/*
+ * Copyright 2018-2025 Henri Tremblay.
+ */
 public class LaunchMultiFile41 {
   public static void main(String[] args) {
     Helper.hello();
