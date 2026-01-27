710f6eb7ea79e8c7defcfb255dfd7354042146fe
Henri Tremblay <henri.tremblay@gmail.com>
2026-01-26 19:31:00 -0500

    Some more examples

--- examples/src/test/java/pro/tremblay/alljava/conf/JFR45.java
+++ examples/src/test/java/pro/tremblay/alljava/conf/JFR45.java
@@ -30,9 +30,17 @@ public class JFR45 {
 // java -XX:StartFlightRecording=filename=app.jfr,settings=profile,dumponexit=true -jar your-app.jar
 
 // PID=$(jcmd -l | grep JFR45 | cut -d' ' -f1)
-// jcmd $PID JFR.start name=on_demand settings=profile
+// jcmd $PID JFR.start name=on_demand settings=profile jdk.ExecutionSample#period=1ms jdk.NativeMethodSample#period=1ms
 // jcmd $PID JFR.check
 // jcmd $PID JFR.dump name=on_demand filename=app.jfr
 // jcmd $PID JFR.stop name=on_demand
 // jfr summary app.jfr
 // jfr print --events CPULoad,JavaErrorThrow app.jfr
+
+// jcmd $PID JFR.start name=on_demand settings=profile jdk.MethodTrace#filter=java.lang.Integer::toString
+// jcmd $PID JFR.dump name=on_demand filename=app.jfr
+// jfr view --cell-height 5 MethodTrace app.jfr
+// -XX:StartFlightRecording:method-timing=::<clinit>
+
+// CPU-time profiling: java -XX:StartFlightRecording=jdk.CPUTimeSample#enabled=true,filename=profile.jfr -cp examples/target/test-classes pro.tremblay.alljava.conf.JFR45
+// Lower sampling: java -XX:StartFlightRecording=jdk.ExecutionSample#period=10ms,jdk.NativeMethodSample#period=10ms,jdk.CPUTimeSample#period=10msfilename=profile.jfr -cp examples/target/test-classes pro.tremblay.alljava.conf.JFR45
